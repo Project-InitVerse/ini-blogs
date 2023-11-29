01bcd7f96df69ba8d1b259fad35090796d46e064
xiadd <xiadd0102@gmail.com>
2023-11-29 15:00:43 +0800

    Use Hono to serve api

--- scripts/worker.mjs
+++ scripts/worker.mjs
@@ -7,7 +7,11 @@ app.use("/api/*", cors());
 
 app.get("/api/blogs/", async (c) => {
   const blogs = await c.env.INI_BLOGS.get("blogs");
-  return c.json({ blogs });
+  let data = [];
+  try {
+    data = JSON.parse(blogs);
+  } catch (e) {}
+  return c.json({ data });
 });
 
 export default app;
