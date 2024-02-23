7a18d388f18caa23d071e1f330448bda8a574475
xiadd <xiadd0102@gmail.com>
2024-02-23 22:32:36 +0800

    render md to html

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -2,6 +2,17 @@ import fs from "fs";
 import { globby } from "globby";
 import matter from "gray-matter";
 
+import MarkdownIt from "markdown-it";
+import MarkdownItVideo from "markdown-it-video";
+
+const md = new MarkdownIt({
+  html: true,
+  linkify: true,
+  typography: true,
+}).use(MarkdownItVideo, {
+  youtube: { width: "100%", height: 500 },
+});
+
 const blogs = [];
 
 const mdFiles = await globby("blogs", {
@@ -14,6 +25,7 @@ for (const mdFile of mdFiles) {
   const mdContent = fs.readFileSync(mdFile, "utf-8");
   const data = matter(mdContent);
   data.path = mdFile.replace("blogs/", "").replace(".md", "");
+  data.content = md.render(data.content);
   blogs.push(data);
 }
 
